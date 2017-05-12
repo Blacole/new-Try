bd024dbdd8e47a18b53dfd40a0486e15026f8585
jiyouliang <18098975621@163.com>
2017-05-12 15:44:39 +0800

    first commit

---
+++ app/src/main/java/com/itheima/singleintanceleak/HttpUtls.java
@@ -0,0 +1,27 @@
+package com.itheima.singleintanceleak;
+
+import android.content.Context;
+
+/**
+ * Created by youliang.ji on 2017/5/12.
+ */
+
+public class HttpUtls {
+    private static HttpUtls instance;
+    private static Context mContext;
+    private HttpUtls(){}
+
+    public static HttpUtls getInstance(Context context){
+        //单例
+        if(instance == null){
+            synchronized (HttpUtls.class){
+                if(instance == null){
+                    instance = new HttpUtls();
+                    mContext = context;
+                }
+            }
+        }
+        return instance;
+    }
+
+}
---
+++ app/src/main/java/com/itheima/singleintanceleak/MainActivity.java
@@ -0,0 +1,21 @@
+package com.itheima.singleintanceleak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 使用分支管理检测内存泄漏
+ *
+ * 1.发布到应用市场的版本：不带leak -> master 主分支
+ * 2.发布之前检测内存泄漏：集成leak -> 独立分支处理
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HttpUtls.getInstance(this);
+    }
+}
